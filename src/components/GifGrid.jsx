f833ae527ec91ffcf4d08c1ca05189163a3f92d4
Freddy Mendoza <freddy.mendoza@gmail.com>
2022-10-17 14:32:02 -0500

    mostrar título de las imágenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import { getGifs } from "./helpers/getGifs";
 
 export const GifGrid = ({category}) => {
 
-    getGifs(category);
+    const [images, setImages] = useState([]);
+
+    const getImages = async() => {
+        const newImages = await getGifs(category); //getGifs tiene la data
+        setImages(newImages);
+    }
+
+    //se aplica el hook useEffect para cargar una sola vez el helper fetch de la data
+    useEffect(() => {
+        getImages();
+    }, []);
+
+    // console.log(images);
 
     return (
         <>
             <h3>{category}</h3>
-            <p>Hola mundo</p>
+            <ol>
+                {
+                    // images.map(image => (
+                    //     <li key={image.id}>{image.title}</li>
+                    // ))
+                    images.map( ({id,title}) => (
+                        <li key={id}>{title}</li>
+                    ))
+                }
+            </ol>
         </>
     )
 }
--- src/components/helpers/getGifs.js
+++ src/components/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async(category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=APH9rT0CtFAzveESknwNgzkMMmLz8fQO&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=APH9rT0CtFAzveESknwNgzkMMmLz8fQO&q=${category}&limit=10`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     // console.log(data);
@@ -11,6 +11,6 @@ export const getGifs = async(category) => {
         url: img.images.downsized_medium.url,
     }))
 
-    console.log(gifs);
+    // console.log(gifs);
     return gifs;
 }
