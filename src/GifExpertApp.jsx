d7c38f8b4d677258e289b9ef3c241a2d31b96562
Freddy Mendoza <freddy.mendoza@gmail.com>
2022-10-17 08:54:05 -0500

    ejemplo botón agregar nueva categoría al estado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,7 +3,16 @@ import {useState} from 'react';
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch','Dragon Ball']);
-    console.log(categories);
+    // console.log(categories);
+    
+    //agregar una categoría al estado
+    const onAddCategory = () => {
+        // console.log('Valorant');
+        // se usa setCategories para trabajar con categories
+        // se trae el array que tenía con ...categories y se agrega el nuevo
+        setCategories(['Valorant', ...categories]); //opción 1
+        // setCategories(cat=>[...cat,'Valorant']); //opción 2
+    }
 
     return (
         <>
@@ -13,6 +22,7 @@ export const GifExpertApp = () => {
             {/* Input */}
 
             {/* Listado de elementos */}
+            <button onClick={onAddCategory}>Agregar</button>
             <ol>
                 {categories.map(category => {
                     return <li key={category}>{category}</li>
